abc6a7382578e59253c96d073b15ca0de3fe663e
Ravi Shankar Singh <civilstudy2024@gmail.com>
2024-12-14 01:14:44 +0530

    info div add

--- Protfolio/src/index.js
+++ Protfolio/src/index.js
@@ -2,11 +2,58 @@ document.addEventListener('DOMContentLoaded', () => {
     let menu = document.querySelector('#header ul');
     let bar = document.querySelector('#header .fa-bars');
 
-    bar.addEventListener('click', () => {
+    bar.addEventListener('click', function(){
         menu.classList.toggle("show");
         console.log("Menu toggled");
     });
 });
+
+const aboutLink = document.querySelector('#about-link');
+const servicesLink = document.querySelector('#services-link');
+const blogLink = document.querySelector('#blog-link');
+const contactLink = document.querySelector('#contact-link');
+
+
+const infoBox = document.querySelector('.info');
+const servicesBox = document.querySelector('.services-info');
+const blogBox = document.querySelector('.blog-info');
+const contactBox = document.querySelector('.contact-info');
+
+const closeButtons = document.querySelectorAll('.fa-xmark');
+
+aboutLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    infoBox.classList.toggle('show');
+    console.log("Info box toggled");
+});
+
+servicesLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    servicesBox.classList.toggle('show');
+    console.log("Services box toggled");
+});
+
+blogLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    blogBox.classList.toggle('show');
+    console.log("Blog box toggled");
+});
+
+contactLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    contactBox.classList.toggle('show');
+    console.log("Contact box toggled");
+});
+
+closeButtons.forEach(button => {
+    button.addEventListener('click', function () {
+        this.parentElement.classList.remove('show');
+        console.log("Info box closed");
+    });
+});
+
+
+
 
 const colors = ["#d20962", "#0ebeff", "#7ac143", "#00a78e", "#00bce4", "#8e43e7", "#005be2",];
 const numballs = 30;
