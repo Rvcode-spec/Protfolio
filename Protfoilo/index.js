89d8577b1a17712cc44c18dcfcd1b26b1c0ddc6e
Ravi Shankar Singh <civilstudy2024@gmail.com>
2024-12-15 23:09:59 +0530

    All Icons Completed

--- Protfoilo/index.js
+++ Protfoilo/index.js
@@ -21,3 +21,13 @@ const scrollButton = document.getElementById("scrollButton");
   closeInfo.addEventListener("click", () => {
     infoItem.classList.add("hidden");
   });
+
+
+  const sunIcon = document.getElementById("sun-icon");
+
+    sunIcon.addEventListener("click", () => {
+      // Generate a random color
+      const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+      // Change the background color of the body
+      document.body.style.backgroundColor = randomColor;
+    });
